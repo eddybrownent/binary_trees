21987ea33cf98e5968d93a6498668badc074523b
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-08-02 22:50:42 +0300

    created a function that measures the balance factor of a binary tree

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Fuction that measures height a binary tree
+ * @tree: The tree to work on
+ *
+ * Return: The height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+	{
+		return (-1);
+	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
+ * binary_tree_balance - Function to measure balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the balnce factor
+ *
+ * Return: If tree is NULL return 0
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (left_height - right_height);
+}
